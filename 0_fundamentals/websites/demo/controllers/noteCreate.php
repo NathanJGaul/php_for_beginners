678d8804e380a9ab14268c10bcad79ef2160d673
Nathan Gaul <nathanjgaul@gmail.com>
2026-01-14 17:31:27 -0600

    Basic form validation

--- 0_fundamentals/websites/demo/controllers/noteCreate.php
+++ 0_fundamentals/websites/demo/controllers/noteCreate.php
@@ -6,10 +6,21 @@ $db = new Database($config['database'], 'root', '');
 $heading = "Create Note";
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  $db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)', [
-    'body' => $_POST['body'],
-    'user_id' => 1
-  ]);
+  $errors = [];
+  $body = $_POST['body'];
+
+  if (strlen($body) === 0) $errors['body'] = 'A body is required';
+
+  if (strlen($body) > 100) $errors['body'] = 'Maximum note length is 100 characters';
+
+  if (empty($errors)) {
+    $db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)', [
+        'body' => $_POST['body'],
+        'user_id' => 1
+      ]);
+  }
+
+  
 }
 
 require 'views/noteCreate.view.php';
--- 0_fundamentals/websites/demo/views/noteCreate.view.php
+++ 0_fundamentals/websites/demo/views/noteCreate.view.php
@@ -10,7 +10,10 @@
       <div class="col-span-full">
           <label for="body" class="block text-sm/6 font-medium text-white">Body</label>
           <div class="mt-2">
-            <textarea id="body" name="body" rows="3" class="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"></textarea>
+            <textarea id="body" name="body" rows="3" class="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"><?= $_POST['body'] ?? '' ?></textarea>
+            <?php if (isset($errors['body'])) : ?>
+              <p class="text-red-300 text-xs mt-6"><?= $errors['body'] ?></p>
+            <?php endif; ?>
           </div>
           <!-- <p class="mt-3 text-sm/6 text-gray-400">Cre</p> -->
         </div>
